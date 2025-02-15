e9bb26f155c223460ceaf5c49ef8cb56ddc0ce23
Yared <149290215+yared-Habtamu@users.noreply.github.com>
2025-02-15 22:08:46 +0300

    Create task2.py

---
+++ week-2/task2.py
@@ -0,0 +1,14 @@
+def summ(arr):
+    return sum(arr)
+print("EVENS")
+for num in range(1,21):
+    if num%2==0:
+        print(num,end=' ')
+
+
+def maximum(arr):
+    return max(arr)
+
+array=[1,2,4,6,8,9,3]
+print("\nSUM= ",summ(array))
+print("Max= ",maximum(array))
